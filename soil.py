b02e5bcb3b9aa23df9b85e4d268a4d4f4c1b296a
Haamid Raza Kazi <haamidraza03@gmail.com>
2023-04-06 22:33:04 +0530

    soilwebscap

---
+++ soil.py
@@ -0,0 +1,16 @@
+
+
+import requests
+from bs4 import BeautifulSoup
+import lxml
+import re
+
+html = requests.get("https://agriculture.vic.gov.au/farm-management/soil/what-is-soil")
+soup = BeautifulSoup(html.text, "lxml")
+
+with open("soil.txt", "w", encoding="utf-8") as output:
+    output.write(soup.prettify())
+
+
+soil = soup.findAll('main', class_ = "")
+print(soil)
