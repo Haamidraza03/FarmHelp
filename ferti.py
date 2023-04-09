b5b7af11ef01e68c09a69812ef5154317924fbbc
Haamid Raza Kazi <haamidraza03@gmail.com>
2023-04-10 01:21:46 +0530

    2genlnkdn

---
+++ ferti.py
@@ -0,0 +1,14 @@
+import requests
+from bs4 import BeautifulSoup
+import lxml
+import re
+
+html = requests.get("https://www.britannica.com/topic/fertilizer")
+soup = BeautifulSoup(html.text, "lxml")
+
+with open("ferti.txt", "w", encoding="utf-8") as output:
+    output.write(soup.prettify())
+
+
+soil = soup.findAll('div', class_ = 'topic-content pt-15')
+print(soil)
